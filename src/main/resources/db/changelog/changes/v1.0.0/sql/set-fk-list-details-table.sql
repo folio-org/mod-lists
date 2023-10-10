66d03c82079099d2a3e89ade401a3ed51d678450
Kriti Jain <kjain@ebsco.com>
2023-10-10 12:11:08 -0400

    kjain/ebsco/MODLISTS-29:sorting delpoyment issue

--- src/main/resources/db/changelog/changes/v1.0.0/sql/set-fk-list-details-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/set-fk-list-details-table.sql
@@ -1,6 +1,9 @@
+ALTER TABLE list_details DROP CONSTRAINT IF EXISTS fk_in_progress_refresh;
 ALTER TABLE list_details ADD CONSTRAINT fk_in_progress_refresh FOREIGN KEY (in_progress_refresh_id)
                                     REFERENCES list_refresh_details (id) MATCH SIMPLE ;
+ALTER TABLE list_details DROP CONSTRAINT IF EXISTS fk_success_refresh;
 ALTER TABLE list_details ADD CONSTRAINT fk_success_refresh FOREIGN KEY (success_refresh_id)
                                     REFERENCES list_refresh_details (id) MATCH SIMPLE ;
+ALTER TABLE list_details DROP CONSTRAINT IF EXISTS fk_failed_refresh;
 ALTER TABLE list_details ADD CONSTRAINT fk_failed_refresh FOREIGN KEY (failed_refresh_id)
                                     REFERENCES list_refresh_details (id) MATCH SIMPLE ;
