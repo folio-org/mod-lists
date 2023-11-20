6a3081eb964c69fdd156a244dba98cb77045d3d4
Noah Overcash <novercash@ebsco.com>
2023-11-20 17:17:45 -0500

    try importing config?

--- src/main/java/org/folio/list/ModListsApplication.java
+++ src/main/java/org/folio/list/ModListsApplication.java
@@ -1,6 +1,8 @@
 package org.folio.list;
 
+import org.folio.fql.config.FqlConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,10 +12,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableFeignClients
 @ConfigurationPropertiesScan
+@ImportAutoConfiguration(classes = { FqlConfiguration.class })
 public class ModListsApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ModListsApplication.class, args);
   }
-
 }
