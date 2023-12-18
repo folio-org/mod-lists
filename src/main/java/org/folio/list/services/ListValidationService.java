4d0eb713d0cce6cf4194817ca6aad288db42681b
Noah Overcash <novercash@ebsco.com>
2023-12-18 18:44:21 -0500

    oops

--- src/main/java/org/folio/list/services/ListValidationService.java
+++ src/main/java/org/folio/list/services/ListValidationService.java
@@ -64,7 +64,6 @@ public class ListValidationService {
 
   public void assertSharedOrOwnedByUser(ListEntity list, ListActions failedAction) {
     UUID currentOwnerId = (list.getUpdatedBy() == null) ? list.getCreatedBy() : list.getUpdatedBy();
-    System.out.println(folioExecutionContext.getUserId());
     if (Boolean.TRUE.equals(list.getIsPrivate()) && ! currentOwnerId.equals(folioExecutionContext.getUserId())) {
       throw new PrivateListOfAnotherUserException(list, failedAction);
     }
