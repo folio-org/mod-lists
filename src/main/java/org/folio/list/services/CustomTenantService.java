374f9116138b710f4884ddb4bb77594affc8ce30
Noah Overcash <novercash@ebsco.com>
2024-05-16 13:18:28 -0400

    add logging?

--- src/main/java/org/folio/list/services/CustomTenantService.java
+++ src/main/java/org/folio/list/services/CustomTenantService.java
@@ -1,14 +1,18 @@
 package org.folio.list.services;
 
+import lombok.extern.log4j.Log4j2;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.spring.service.TenantService;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
+@Primary
 @Service
 public class CustomTenantService extends TenantService {
 
@@ -27,6 +31,7 @@ public class CustomTenantService extends TenantService {
 
   @Override
   protected void afterTenantUpdate(TenantAttributes tenantAttributes) {
+    log.info("Initializing system user");
     prepareSystemUserService.setupSystemUser();
   }
 }
