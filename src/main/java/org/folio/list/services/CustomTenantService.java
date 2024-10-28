ada2ec1debf42fbcb74fef90a2ae262b3227a1ee
Matt Weaver <mweaver@ebsco.com>
2024-10-28 13:32:01 -0700

    MODLISTS-155 MODFQMMGR-550 Add retry logic to tenant initialization
    This commit adds retry logic to the tenant initialization service,
    around the query migration operation, since the necessary permissions on
    the system user can take a bit of time when initially creating a eureka
    tenant

--- src/main/java/org/folio/list/services/CustomTenantService.java
+++ src/main/java/org/folio/list/services/CustomTenantService.java
@@ -1,6 +1,7 @@
 package org.folio.list.services;
 
 import lombok.extern.log4j.Log4j2;
+import org.folio.list.exception.InsufficientEntityTypePermissionsException;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
@@ -9,7 +10,11 @@ import org.folio.tenant.domain.dto.TenantAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Log4j2
 @Primary
@@ -37,7 +42,22 @@ public class CustomTenantService extends TenantService {
     log.info("Initializing system user");
     prepareSystemUserService.setupSystemUser();
 
-    log.info("Verifying lists are up to date");
-    migrationService.verifyListsAreUpToDate();
+    // In Eureka, the system user often takes a short bit of time for its permissions to be assigned, so retry in the
+    // case of failures related to missing permissions
+    RetryTemplate.builder()
+      .retryOn(InsufficientEntityTypePermissionsException.class)
+      .exponentialBackoff(Duration.of(2, ChronoUnit.SECONDS), 1.5, Duration.of(1, ChronoUnit.MINUTES))
+      .withTimeout(Duration.of(2, ChronoUnit.MINUTES))
+      .build()
+      .execute(ctx -> {
+        log.info("Verifying lists are up to date. Attempt #" + (ctx.getRetryCount() + 1));
+        migrationService.verifyListsAreUpToDate();
+        return null;
+      }, ctx -> {
+        log.error("Unable to verify lists are up to date", ctx.getLastThrowable());
+        // This RetryTemplate only deals with InsufficientEntityTypePermissionsException, so we know that's what this
+        // Throwable really is. Rethrow it to fail the tenant update process.
+        throw (InsufficientEntityTypePermissionsException) ctx.getLastThrowable();
+      });
   }
 }
