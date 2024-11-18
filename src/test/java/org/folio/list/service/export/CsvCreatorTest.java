0ea81693146f9a0b45aa399a65f5ae000f5f9144
Bobby Sharp <bsharp@ebsco.com>
2024-11-18 10:32:10 -0500

    More tests

--- src/test/java/org/folio/list/service/export/CsvCreatorTest.java
+++ src/test/java/org/folio/list/service/export/CsvCreatorTest.java
@@ -8,7 +8,7 @@ import org.folio.list.domain.ListEntity;
 import org.folio.list.repository.ListContentsRepository;
 import org.folio.list.repository.ListExportRepository;
 import org.folio.list.rest.EntityTypeClient;
-import org.folio.list.rest.QueryClient;
+import org.folio.list.rest.SystemUserQueryClient;
 import org.folio.list.services.ListActions;
 import org.folio.list.services.export.CsvCreator;
 import org.folio.list.services.export.ExportLocalStorage;
@@ -43,7 +43,7 @@ class CsvCreatorTest {
   @Mock
   private EntityTypeClient entityTypeClient;
   @Mock
-  private QueryClient queryClient;
+  private SystemUserQueryClient queryClient;
   @Mock
   private ListExportRepository listExportRepository;
   @Mock(mockMaker = MockMakers.INLINE)
