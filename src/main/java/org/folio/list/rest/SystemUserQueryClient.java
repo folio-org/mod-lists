add6324dfcdb92fe025af1c52b000fa874f192fe
Bobby Sharp <bsharp@ebsco.com>
2024-11-22 10:19:54 -0500

    Experiment with old feign config

--- src/main/java/org/folio/list/rest/SystemUserQueryClient.java
+++ src/main/java/org/folio/list/rest/SystemUserQueryClient.java
@@ -2,6 +2,7 @@ package org.folio.list.rest;
 
 import org.folio.list.configuration.SystemUserFeignConfig;
 import org.folio.querytool.domain.dto.ContentsRequest;
+import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "query/contents/privileged", configuration = SystemUserFeignConfig.class)
+@FeignClient(name = "query/contents/privileged", configuration = FeignClientConfiguration.class)
 public interface SystemUserQueryClient {
 
   @PostMapping("")
