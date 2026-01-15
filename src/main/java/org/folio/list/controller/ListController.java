67436b2beef0fb49da51b3a46239b030f3267db2
Noah Overcash <novercash@ebsco.com>
2026-01-15 17:06:12 -0500

    yikes3

--- src/main/java/org/folio/list/controller/ListController.java
+++ src/main/java/org/folio/list/controller/ListController.java
@@ -46,7 +46,7 @@ public class ListController implements ListApi {
       providedTimestamp = null;
     }
 
-    if (offset == 22) {
+    if (offset == 24) {
       size--;
       providedTimestamp = providedTimestamp.minusDays(2);
     }
