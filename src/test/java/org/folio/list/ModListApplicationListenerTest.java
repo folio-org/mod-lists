5a4593fa7687278a31751c2d9314451208f18618
bvsharp <97990858+bvsharp@users.noreply.github.com>
2026-01-30 15:04:24 -0500

    [MODLISTS-231] Create S3 bucket on startup if it doesn't exist (#271)

--- src/test/java/org/folio/list/ModListApplicationListenerTest.java
+++ src/test/java/org/folio/list/ModListApplicationListenerTest.java
@@ -1,16 +1,20 @@
 package org.folio.list;
 
 import org.folio.s3.client.FolioS3Client;
+import org.folio.s3.exception.S3ClientException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.InputStream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 
 class ModListApplicationListenerTest extends ModListApplicationTest {
   @Test
@@ -22,5 +26,28 @@ class ModListApplicationListenerTest extends ModListApplicationTest {
     ReflectionTestUtils.setField(listsApplicationListener, "s3CheckEnabled", true);
 
     assertThrows(RuntimeException.class, () -> listsApplicationListener.onApplicationEvent(null));
+  }
+
+  @Test
+  void failedBucketInitializationShouldPreventStartup() {
+    FolioS3Client folioS3Client = mock(FolioS3Client.class);
+    Mockito.doThrow(new RuntimeException()).when(folioS3Client).createBucketIfNotExists();
+
+    ListsApplicationListener listsApplicationListener = new ListsApplicationListener(folioS3Client);
+    ReflectionTestUtils.setField(listsApplicationListener, "s3CheckEnabled", true);
+
+    assertThrows(S3ClientException.class, () -> listsApplicationListener.onApplicationEvent(null));
+    Mockito.verify(folioS3Client, times(1)).createBucketIfNotExists();
+  }
+
+  @Test
+  void bucketInitializationShouldBeSkippedWhenStartupCheckIsDisabled() {
+    FolioS3Client folioS3Client = mock(FolioS3Client.class);
+
+    ListsApplicationListener listsApplicationListener = new ListsApplicationListener(folioS3Client);
+    ReflectionTestUtils.setField(listsApplicationListener, "s3CheckEnabled", false);
+
+    assertDoesNotThrow(() -> listsApplicationListener.onApplicationEvent(null));
+    Mockito.verify(folioS3Client, never()).createBucketIfNotExists();
   }
 }
