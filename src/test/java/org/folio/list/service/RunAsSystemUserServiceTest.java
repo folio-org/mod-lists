95d8be173382fca2ef96a634ca9e7668be40757f
Bobby Sharp <97990858+bvsharp@users.noreply.github.com>
2026-06-12 15:17:16 -0400

    remove junit4 usages

--- src/test/java/org/folio/list/service/RunAsSystemUserServiceTest.java
+++ src/test/java/org/folio/list/service/RunAsSystemUserServiceTest.java
@@ -1,7 +1,7 @@
 package org.folio.list.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.Collections;
 import java.util.List;
